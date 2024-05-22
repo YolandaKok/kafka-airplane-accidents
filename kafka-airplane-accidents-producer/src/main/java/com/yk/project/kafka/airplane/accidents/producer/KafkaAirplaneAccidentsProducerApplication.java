ce6d173ab36d4b07fd14ef61eb86a748cdd34f17
YolandaKok <yolanda.kokkinou@gmail.com>
2024-05-22 17:46:02 +0300

    initialize project

---
+++ kafka-airplane-accidents-producer/src/main/java/com/yk/project/kafka/airplane/accidents/producer/KafkaAirplaneAccidentsProducerApplication.java
@@ -0,0 +1,13 @@
+package com.yk.project.kafka.airplane.accidents.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAirplaneAccidentsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAirplaneAccidentsProducerApplication.class, args);
+	}
+
+}
