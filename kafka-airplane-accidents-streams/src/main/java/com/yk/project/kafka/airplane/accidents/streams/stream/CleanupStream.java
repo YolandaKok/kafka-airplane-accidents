6e0c95d76868b5c846e1bda4b0638bcbd3dbe011
YolandaKok <yolanda.kokkinou@gmail.com>
2024-05-23 23:07:13 +0300

    filter null and empty records

--- kafka-airplane-accidents-streams/src/main/java/com/yk/project/kafka/airplane/accidents/streams/stream/CleanupStream.java
+++ kafka-airplane-accidents-streams/src/main/java/com/yk/project/kafka/airplane/accidents/streams/stream/CleanupStream.java
@@ -2,10 +2,12 @@ package com.yk.project.kafka.airplane.accidents.streams.stream;
 
 import com.yk.project.kafka.airplane.accidents.base.model.Accident;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,9 +26,14 @@ public class CleanupStream {
         var accidentSerde = new JsonSerde<>(Accident.class);
 
         var cleanUpStream = builder.stream(rawTopic, Consumed.with(Serdes.Long(), accidentSerde))
-                .filter((k, v) -> !v.getSpeciesName().isBlank());
+                .filter((k, v) -> v.getSpeciesName() != null && !v.getSpeciesName().isBlank())
+                .filter((k, v) -> v.getSpeciesQuantity()!= null && !v.getSpeciesQuantity().isBlank())
+                .map((key, value) -> {
+                    value.setSpeciesQuantity("88");
+                    return new KeyValue<>(key, value);
+                });
 
-        cleanUpStream.to(cleanupTopic);
+        cleanUpStream.to(cleanupTopic, Produced.with(Serdes.Long(), accidentSerde));
         cleanUpStream.print(Printed.toSysOut());
 
         return cleanUpStream;
