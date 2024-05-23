4251864e984192cb3a3043e79014b325f7a3a97b
YolandaKok <yolanda.kokkinou@gmail.com>
2024-05-23 21:32:39 +0300

    kafka streams project

---
+++ kafka-airplane-accidents-streams/src/main/java/com/yk/project/kafka/airplane/accidents/streams/KafkaAirplaneAccidentsStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.yk.project.kafka.airplane.accidents.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAirplaneAccidentsStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAirplaneAccidentsStreamsApplication.class, args);
+	}
+
+}
