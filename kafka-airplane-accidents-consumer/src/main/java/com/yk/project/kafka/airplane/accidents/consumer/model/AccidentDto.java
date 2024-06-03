6c85778587a6ea6b0baaf075182dea23ea987caf
YolandaKok <yolanda.kokkinou@gmail.com>
2024-06-03 23:53:35 +0300

    assembler interface from mapper

--- kafka-airplane-accidents-consumer/src/main/java/com/yk/project/kafka/airplane/accidents/consumer/model/AccidentDto.java
+++ kafka-airplane-accidents-consumer/src/main/java/com/yk/project/kafka/airplane/accidents/consumer/model/AccidentDto.java
@@ -1,17 +1,20 @@
 package com.yk.project.kafka.airplane.accidents.consumer.model;
 
 import com.univocity.parsers.annotations.Parsed;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.redis.core.RedisHash;
 
-import java.io.Serializable;
-
-@RedisHash("AccidentResult")
-@Builder
+@AllArgsConstructor
 @Data
-public class AccidentResult implements Serializable {
-    private String id;
+@Builder
+public class AccidentDto {
+    @Parsed
+    private Integer year;
+    @Parsed
+    private Integer ranking;
+    @Parsed
     private String speciesName;
+    @Parsed
     private Long count;
 }
---
+++ kafka-airplane-accidents-consumer/src/main/java/com/yk/project/kafka/airplane/accidents/consumer/web/AccidentResultController.java
@@ -0,0 +1,40 @@
+package com.yk.project.kafka.airplane.accidents.consumer.web;
+
+import com.univocity.parsers.csv.CsvParserSettings;
+import com.univocity.parsers.csv.CsvRoutines;
+import com.yk.project.kafka.airplane.accidents.consumer.mapper.AccidentMapper;
+import com.yk.project.kafka.airplane.accidents.consumer.model.AccidentDto;
+import com.yk.project.kafka.airplane.accidents.consumer.model.AccidentResult;
+import com.yk.project.kafka.airplane.accidents.consumer.service.AccidentService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.List;
+
+@RestController
+@RequestMapping("/results")
+public class AccidentResultController {
+    @Value("${csv.export.file.path}")
+    private String csvExportFilePath;
+    private final AccidentService accidentService;
+    private final AccidentMapper accidentMapper;
+
+    public AccidentResultController(AccidentService accidentService, AccidentMapper accidentMapper) {
+        this.accidentService = accidentService;
+        this.accidentMapper = accidentMapper;
+    }
+
+    @GetMapping
+    public List<AccidentResult> getResults() {
+        var results = accidentMapper.assembleAll(accidentService.findAll());
+        new CsvRoutines()
+                .writeAll(results, AccidentDto.class,
+                        new File(csvExportFilePath));
+
+        return accidentService.findAll();
+    }
+}
